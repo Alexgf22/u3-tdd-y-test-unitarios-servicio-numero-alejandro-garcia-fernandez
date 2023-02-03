289c51a27c1ff0a98ed6261cc50746a28a7e87d6
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-02-03 08:38:33 +0100

    tests pasados

--- src/test/kotlin/es/edu/iesra/daw/agf/api/ServicioNumerosTest.kt
+++ src/test/kotlin/es/edu/iesra/daw/agf/api/ServicioNumerosTest.kt
@@ -2,8 +2,9 @@ package es.edu.iesra.daw.agf.api
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.ints.shouldBeExactly
-import io.kotest.matchers.should
 import io.kotest.matchers.string.shouldStartWith
 
 class ServicioNumerosTest : DescribeSpec({
@@ -38,8 +39,39 @@ class ServicioNumerosTest : DescribeSpec({
         it("El servicio quedaría vacío, al sacar el primer y único número") {
             servicio.restoNumeros().size.shouldBeExactly(0)
         }
+    }
+
+    /*describe("Una vez inicializado, por segunda vez") {
+        servicio.inicializa()
+        it("El servicio contiene únicamente el valor mínimo") {
+            servicio.dameUnNumero()?.shouldBeExactly(minimo)
+        }
+
+        it("El segundo valor debe ser nulo") {
+            servicio.dameUnNumero()?.let {
+                it.shouldBeNull()
+            }
+        }
+    }*/
 
 
+
+    describe("Con una diferencia de 2 entre máximo y mínimo") {
+        val minimo = 5
+        val maximo = 7
+        val servicio = ServicioNumeros(minimo, maximo)
+        it("El servicio debe contener los elementos {minimo , maximo -1}") {
+            servicio.restoNumeros().shouldContainAll(minimo, maximo -1)
+        }
+    }
+
+    describe("Debe devolver números") {
+        it("Todos los números entre mínimo y máximo") {
+            val minimo = 1
+            val maximo = 1550
+            val servicio = ServicioNumeros(minimo, maximo)
+            servicio.restoNumeros().shouldContainExactlyInAnyOrder(List(maximo - minimo) {minimo + it})
+        }
     }
 
 })
