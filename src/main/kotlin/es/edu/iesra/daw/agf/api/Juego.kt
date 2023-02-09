3622b58a89a6594f9f4d783b5e72e5dda99c9d5b
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-02-09 11:01:06 +0100

    borrador clase Juego

--- src/main/kotlin/es/edu/iesra/daw/agf/api/Juego.kt
+++ src/main/kotlin/es/edu/iesra/daw/agf/api/Juego.kt
@@ -1,48 +1,32 @@
 package es.edu.iesra.daw.agf.api
 
-class Juego {
+class Juego(private val numeroCartones: Int) {
+    private val cartones = mutableListOf<GeneradorDeCartones.Carton>()
+    private val bombo = Locutor.Bombo()
 
-    private var cartones: MutableList<GeneradorDeCartones.Carton> = mutableListOf()
-
-    private var bombo = Locutor.Bombo()
-
-    fun agregarCarton(carton: GeneradorDeCartones.Carton) {
-        cartones.add(carton)
-    }
-
-
-    fun play() {
-
-    }
-
-    fun comprobarLinea(): Boolean {
-        for (carton in cartones) {
-            if (carton.comprobarLinea()) {
-                println("El cartón con el id: ${carton.id} ha obtenido una línea!")
-                return true
-            }
-        }
-        return false
-    }
-
-
-
-
-    fun comprobarBingo() {
-        for (carton in cartones) {
-            if (carton.comprobarBingo()) {
-                println("BINGO en el cartón de ${carton.id}")
-            }
+    init {
+        for (i in 0 until numeroCartones) {
+            cartones.add(GeneradorDeCartones.Carton(i + 1))
         }
     }
 
-
-    override fun toString(): String {
-        return super.toString()
+    fun jugar() {
+        while (cartones.none { it.haGanado() }) {
+            val numero = bombo.extraerBola()
+            for (carton in cartones) {
+                carton.marcarNumero(numero)
+                if (carton.haGanadoLínea()) {
+                    println("Carton ${carton.id} ha ganado una línea")
+                }
+            }
+        }
+        val ganador = cartones.first { it.haGanado() }
+        println("Carton ${ganador.id} ha ganado el bingo")
     }
 
-
-
-
-
+    fun obtenerResumen() {
+        for (carton in cartones) {
+            println("Carton ${carton.id}: Líneas ganadas ${carton.lineasGanadas()}, Bingo: ${carton.haGanado()}")
+        }
+    }
 }
